e7d71e2fec46926da21e30d15475f66c6fc9ff2a
养乐多要吸管 <a102467@outlook.com>
2023-12-31 14:17:17 +0800

    Multiple permission verification

--- drfdemo/per.py
+++ drfdemo/per.py
@@ -2,8 +2,10 @@ import random
 
 from rest_framework.permissions import BasePermission
 
-
+# 权限组件
 class MyPermission(BasePermission):
+    message = {"code": 1003, "msg": "无权限访问"}
+
     def has_permission(self, request, view):
         # 获取请求中的数据，然后进行校验....
         v1 = random.randint(1,3)
